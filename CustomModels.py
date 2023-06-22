65d141ebb7d1b6a13b5011ee016d8d04ad8dfd66
Bart <kmiecikjob@gmail.com>
2023-06-22 21:13:37 +0200

    Adding multiclass classyfication

--- CustomModels.py
+++ CustomModels.py
@@ -26,3 +26,54 @@ class SimpleLSTM(nn.Module):
     out, _ = self.lstm(x, (h0, c0))
     out = self.finish(out[:, -1, :])
     return out
+
+class MultiClassification(nn.Module):
+  def __init__(self,
+               input_features,
+               output_features,
+               hidden_units):
+    super().__init__()
+    self.combo_layer1 = nn.Sequential(
+        nn.Conv2d(in_channels=input_features,
+                  out_channels=hidden_units,
+                  stride=1,
+                  padding=1,
+                  kernel_size=3),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=hidden_units,
+                  out_channels=hidden_units,
+                  stride=1,
+                  padding=1,
+                  kernel_size=3),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2,
+                     stride=2)
+    )
+    self.combo_layer2 = nn.Sequential(
+        nn.Conv2d(in_channels=hidden_units,
+                  out_channels=hidden_units,
+                  kernel_size=3,
+                  padding=1,
+                  stride=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=hidden_units,
+                  out_channels=hidden_units,
+                  kernel_size=3,
+                  padding=1,
+                  stride=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2, stride=2)
+    )
+    self.classifier = nn.Sequential(
+        nn.Flatten(),
+        nn.Linear(in_features=hidden_units*7*7,
+                  out_features=output_features)
+    )
+
+  def forward(self, x):
+    x = self.combo_layer1(x)
+    #print(x.shape)
+    x = self.combo_layer2(x)
+    #print(x.shape)
+    x = self.classifier(x)
+    return x
